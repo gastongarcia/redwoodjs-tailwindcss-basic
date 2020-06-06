e6f927f8a624b0982b1ac056cd0996e5da181437
Gastón García <gaston.garcia@gmail.com>
2020-06-06 16:14:13 -0600

    Redwood Tailwind basic setup

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-xs">Home</h1>
+      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+    </div>
+  )
+}
+
+export default HomePage
